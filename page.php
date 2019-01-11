da7b58275e23d98d9c70b329974b27252b856872
halfg33k <johnsonam17@gmail.com>
2019-01-11 10:26:46 -0500

    changed page to better fit gutenberg

--- page.php
+++ page.php
@@ -1,12 +1,9 @@
 <?php get_header(); ?>
-    <section class="content">
-      <div class="container">
-        <?php if(have_posts()): while(have_posts()): the_post(); ?>
-          <h1 class="page-title"><?php the_title(); ?></h1>
-          <article>
-            <?php the_content(); ?>
-          </article>
-        <?php endwhile; endif; ?>
-      </div>
-    </section>
+
+    <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
+        
+        <?php the_content(); ?>
+
+    <?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
